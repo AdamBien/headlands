d9527397205a84b90499493f4f903f1b29dff3ab
Adam Bien <abien@adam-bien.com>
2014-12-23 11:08:10 +0100

    Initial creation

---
+++ headlands/src/main/java/com/airhacks/headlands/cache/boundary/EntriesResource.java
@@ -0,0 +1,35 @@
+package com.airhacks.headlands.cache.boundary;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class EntriesResource {
+
+    private CacheDiscoverer discoverer;
+
+    @PathParam("cacheName")
+    @NotNull
+    private String cacheName;
+
+    public EntriesResource(CacheDiscoverer discoverer) {
+        this.discoverer = discoverer;
+    }
+
+    @GET
+    public Response all(@QueryParam("maxEntries") @DefaultValue("100") long maxEntries) {
+        StreamingOutput so = (o) -> {
+            discoverer.dumpInto(cacheName, maxEntries, o);
+        };
+        return Response.ok(so).build();
+    }
+
+}
