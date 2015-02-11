8b521b62cc3301a74e352f8fc174579b6e4aa450
Adam Bien <abien@adam-bien.com>
2015-02-11 10:20:25 +0100

    Additional information provided for caches

--- headlands/src/main/java/com/airhacks/headlands/cache/boundary/CachesResource.java
+++ headlands/src/main/java/com/airhacks/headlands/cache/boundary/CachesResource.java
@@ -6,10 +6,13 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 /**
@@ -30,12 +33,20 @@ public class CachesResource {
     public JsonArray all(@Context UriInfo info) {
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
         List<String> cacheNames = discoverer.cacheNames();
-        cacheNames.stream().map(n -> info.getAbsolutePathBuilder().
-                path(n).
-                build().
-                toASCIIString()).
+        cacheNames.stream().map(n -> createCacheLinks(info, n)).
                 forEach(arrayBuilder::add);
         return arrayBuilder.build();
+    }
+
+    JsonObject createCacheLinks(UriInfo info, String name) {
+        JsonObjectBuilder response = Json.createObjectBuilder();
+        UriBuilder cacheLink = info.getAbsolutePathBuilder().
+                path(name);
+        UriBuilder entries = cacheLink.path("entries");
+        response.add("name", name);
+        response.add("link", cacheLink.build().toASCIIString());
+        response.add("entries", entries.build().toASCIIString());
+        return response.build();
     }
 
     @Path("{cacheName}")
