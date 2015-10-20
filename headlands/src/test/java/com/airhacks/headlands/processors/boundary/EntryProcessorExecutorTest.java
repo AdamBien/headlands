652b548598681b5396d3790bdd398d94efb3abdd
Adam Bien <abien@adam-bien.com>
2015-10-20 19:11:51 +0200

    test fixed

--- headlands/src/test/java/com/airhacks/headlands/processors/boundary/EntryProcessorExecutorTest.java
+++ headlands/src/test/java/com/airhacks/headlands/processors/boundary/EntryProcessorExecutorTest.java
@@ -1,12 +1,17 @@
 package com.airhacks.headlands.processors.boundary;
 
-import javax.cache.processor.EntryProcessor;
-import static org.hamcrest.CoreMatchers.is;
+import com.airhacks.headlands.cache.boundary.CacheDiscoverer;
+import com.airhacks.headlands.engine.control.NashornEngine;
+import java.util.HashSet;
+import java.util.Map;
+import javax.cache.Cache;
+import javax.cache.processor.EntryProcessorResult;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -15,43 +20,27 @@ import org.junit.Test;
 public class EntryProcessorExecutorTest {
 
     EntryProcessorExecutor cut;
+    String existingCache = "dukes";
+    private Cache cache;
 
     @Before
     public void init() {
+        NashornEngine engine = new NashornEngine();
+        engine.initializeEngine();
         this.cut = new EntryProcessorExecutor();
-        this.cut.initializeEngine();
-    }
-
-    @Test
-    public void createProcessorFromEmptyScript() {
-        EntryProcessor<String, String, String> invalid = this.cut.createProcessorFromScript("");
-        assertNull(invalid);
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void createProcessorFromInvalidScript() {
-        EntryProcessor<String, String, String> invalid = this.cut.createProcessorFromScript("INVALID");
-        assertNull(invalid);
+        this.cut.engine = engine;
+        this.cut.discoverer = mock(CacheDiscoverer.class);
+        this.cache = mock(Cache.class);
+        when(this.cut.discoverer.getCache(existingCache)).thenReturn(cache);
     }
 
     @Test
     public void createValidProcessorFromScript() {
         String expected = "works";
         String processorScript = "function process(entry,args){ return \"" + expected + "\"}";
-        EntryProcessor<String, String, String> processor = this.cut.createProcessorFromScript(processorScript);
-        assertNotNull(processor);
-        String result = processor.process(null);
-        assertThat(result, is(expected));
-    }
-
-    @Test
-    public void createValidProcessorWithArgument() {
-        String expected = "works";
-        String processorScript = "function process(entry,arg){ return arg[0]}";
-        EntryProcessor<String, String, String> processor = this.cut.createProcessorFromScript(processorScript);
-        assertNotNull(processor);
-        String result = processor.process(null, expected);
-        assertThat(result, is(expected));
+        Map<String, EntryProcessorResult<String>> result = this.cut.execute(existingCache, processorScript, new HashSet<>(), new HashSet<>());
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
     }
 
 }
