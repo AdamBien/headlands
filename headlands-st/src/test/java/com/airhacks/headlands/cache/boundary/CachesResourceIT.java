3bba249bd49fe370724274c276e5d3d86d928280
Adam Bien <abien@adam-bien.com>
2015-10-20 10:30:13 +0200

    Rulz introduced

--- headlands-st/src/test/java/com/airhacks/headlands/cache/boundary/CachesResourceIT.java
+++ headlands-st/src/test/java/com/airhacks/headlands/cache/boundary/CachesResourceIT.java
@@ -1,17 +1,15 @@
 package com.airhacks.headlands.cache.boundary;
 
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
 import javax.json.Json;
 import javax.json.JsonObject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -20,33 +18,26 @@ import org.junit.Test;
  */
 public class CachesResourceIT {
 
-    private Client client;
-    private WebTarget tut;
-
-    @Before
-    public void init() {
-        this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8080/headlands/resources/caches");
-
-    }
+    @Rule
+    public JAXRSClientProvider tut = JAXRSClientProvider.buildWithURI("http://localhost:8080/headlands/resources/caches");
 
     @Test
     public void crudCaches() {
         String cacheName = "cache-" + System.currentTimeMillis();
-        Response response = this.tut.request(MediaType.APPLICATION_JSON).get();
+        Response response = this.tut.target().request(MediaType.APPLICATION_JSON).get();
         assertThat(response.getStatus(), is(200));
         JsonObject configuration = Json.createObjectBuilder().build();
-        response = this.tut.path(cacheName).request().put(Entity.json(configuration));
+        response = this.tut.target().path(cacheName).request().put(Entity.json(configuration));
         assertThat(response.getStatus(), is(201));
 
-        response = this.tut.path(cacheName).request(MediaType.APPLICATION_JSON).options();
+        response = this.tut.target().path(cacheName).request(MediaType.APPLICATION_JSON).options();
         assertThat(response.getStatus(), is(200));
 
         JsonObject info = response.readEntity(JsonObject.class);
         System.out.println("info = " + info);
         assertNotNull(info);
 
-        response = this.tut.path(cacheName).request().put(Entity.json(configuration));
+        response = this.tut.target().path(cacheName).request().put(Entity.json(configuration));
         assertThat(response.getStatus(), is(200));
     }
 
--- headlands-st/src/test/java/com/airhacks/headlands/cache/boundary/EntriesResourceIT.java
+++ headlands-st/src/test/java/com/airhacks/headlands/cache/boundary/EntriesResourceIT.java
@@ -1,17 +1,16 @@
 package com.airhacks.headlands.cache.boundary;
 
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
 import javax.json.Json;
 import javax.json.JsonObject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.After;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 /**
@@ -20,17 +19,15 @@ import org.junit.Test;
  */
 public class EntriesResourceIT {
 
-    private Client client;
-    private WebTarget tut;
+    @Rule
+    public JAXRSClientProvider tut = JAXRSClientProvider.buildWithURI("http://localhost:8080/headlands/resources/caches");
     private String cacheName;
 
     @Before
     public void init() {
-        this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8080/headlands/resources/caches");
         this.cacheName = "cache-" + System.currentTimeMillis();
         JsonObject configuration = Json.createObjectBuilder().build();
-        this.tut.path(cacheName).request().put(Entity.json(configuration));
+        this.tut.target().path(cacheName).request().put(Entity.json(configuration));
 
     }
 
@@ -38,16 +35,16 @@ public class EntriesResourceIT {
     public void crudEntries() {
         String expectedValue = "java rocks " + System.currentTimeMillis();
         String expectedKey = "status" + System.currentTimeMillis();
-        this.tut.path(cacheName).path("entries").path(expectedKey).request().put(Entity.text(expectedValue));
+        this.tut.target().path(cacheName).path("entries").path(expectedKey).request().put(Entity.text(expectedValue));
 
-        JsonObject cacheContent = this.tut.path(cacheName).path("entries").request().get(JsonObject.class);
+        JsonObject cacheContent = this.tut.target().path(cacheName).path("entries").request().get(JsonObject.class);
         System.out.println("cacheContent = " + cacheContent);
         assertNotNull(cacheContent);
 
-        String actualValue = this.tut.path(cacheName).path("entries").path(expectedKey).request().get(String.class);
+        String actualValue = this.tut.target().path(cacheName).path("entries").path(expectedKey).request().get(String.class);
         assertThat(actualValue, is(expectedValue));
 
-        Response deletion = this.tut.path(cacheName).path("entries").path(expectedKey).request().delete();
+        Response deletion = this.tut.target().path(cacheName).path("entries").path(expectedKey).request().delete();
         assertThat(deletion.getStatus(), is(200));
     }
 
@@ -58,16 +55,16 @@ public class EntriesResourceIT {
 
         JsonObject input = Json.createObjectBuilder().add(expectedKey, expectedValue).build();
 
-        this.tut.path(cacheName).path("entries").request().put(Entity.json(input));
+        this.tut.target().path(cacheName).path("entries").request().put(Entity.json(input));
 
-        JsonObject cacheContent = this.tut.path(cacheName).path("entries").request().get(JsonObject.class);
+        JsonObject cacheContent = this.tut.target().path(cacheName).path("entries").request().get(JsonObject.class);
         System.out.println("cacheContent = " + cacheContent);
         assertNotNull(cacheContent);
 
-        String actualValue = this.tut.path(cacheName).path("entries").path(expectedKey).request().get(String.class);
+        String actualValue = this.tut.target().path(cacheName).path("entries").path(expectedKey).request().get(String.class);
         assertThat(actualValue, is(expectedValue));
 
-        Response deletion = this.tut.path(cacheName).path("entries").path(expectedKey).request().delete();
+        Response deletion = this.tut.target().path(cacheName).path("entries").path(expectedKey).request().delete();
         assertThat(deletion.getStatus(), is(200));
     }
 
@@ -75,24 +72,22 @@ public class EntriesResourceIT {
     public void removeAll() {
         String expectedValue = "java rocks " + System.currentTimeMillis();
         String expectedKey = "should be deleted" + System.currentTimeMillis();
-        this.tut.path(cacheName).path("entries").path(expectedKey).request().put(Entity.text(expectedValue));
+        this.tut.target().path(cacheName).path("entries").path(expectedKey).request().put(Entity.text(expectedValue));
 
-        String actualValue = this.tut.path(cacheName).path("entries").path(expectedKey).request().get(String.class);
+        String actualValue = this.tut.target().path(cacheName).path("entries").path(expectedKey).request().get(String.class);
         assertThat(actualValue, is(expectedValue));
 
-        Response deletion = this.tut.path(cacheName).path("entries").request().delete();
+        Response deletion = this.tut.target().path(cacheName).path("entries").request().delete();
         assertThat(deletion.getStatus(), is(200));
 
-        Response response = this.tut.path(cacheName).path("entries").path(expectedKey).request().get();
+        Response response = this.tut.target().path(cacheName).path("entries").path(expectedKey).request().get();
         assertThat(response.getStatus(), is(204));
 
     }
 
     @After
     public void deleteCache() {
-        this.tut.path(cacheName).request().delete();
-        this.client.close();
-
+        this.tut.target().path(cacheName).request().delete();
     }
 
 }
