0adae7ed76ba015109d1638900baa3d6443f13f5
Adam Bien <abien@adam-bien.com>
2015-11-10 08:35:37 +0100

    Caches listing implemented

--- headlands-ui/src/main/java/com/airhacks/headlands/sync/CacheService.java
+++ headlands-ui/src/main/java/com/airhacks/headlands/sync/CacheService.java
@@ -1,8 +1,19 @@
 package com.airhacks.headlands.sync;
 
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -18,8 +29,44 @@ public class CacheService {
         this.client = ClientBuilder.newClient();
     }
 
-    public void sync(String inputText, String outputText) {
+    public void sync(Consumer<String> progressListener, String fromHost, String toHost) {
+        Set<String> cacheNames = getCacheNames(fromHost);
+        progressListener.accept(cacheNames.size() + " caches found!");
+        cacheNames.forEach(name -> copy(progressListener, name, fromHost, toHost));
 
+    }
+
+    Set<String> getCacheNames(String host) {
+        WebTarget target = getTarget(host);
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        JsonArray cacheNames = response.readEntity(JsonArray.class);
+        List<JsonObject> jsonStringList = cacheNames.getValuesAs(JsonObject.class);
+        return jsonStringList.stream().map(j -> j.getString("name")).collect(Collectors.toSet());
+    }
+
+    WebTarget getTarget(String host) {
+        return this.client.target(host + resource);
+    }
+
+    void copy(Consumer<String> progressListener, String cacheName, String fromHost, String toHost) {
+        final WebTarget toTarget = getTarget(toHost);
+        createCache(toTarget, cacheName);
+        JsonObject dump = toTarget.path(cacheName).path("entries").
+                request(MediaType.APPLICATION_JSON).
+                get(JsonObject.class);
+        progressListener.accept("Dump with " + dump.size() + " downloaded!");
+        dump.keySet().forEach(key -> createEntry(toTarget, cacheName, key, dump.getString(key)));
+        progressListener.accept("Content synced");
+
+    }
+
+    public static Response createEntry(WebTarget caches, String cache, String key, String value) {
+        return caches.path(cache).path("entries").path(key).request().put(Entity.text(value));
+    }
+
+    public static Response createCache(WebTarget target, String cacheName) {
+        JsonObject configuration = Json.createObjectBuilder().build();
+        return target.path(cacheName).request().put(Entity.json(configuration));
     }
 
 }
--- headlands-ui/src/main/java/com/airhacks/headlands/sync/SyncPresenter.java
+++ headlands-ui/src/main/java/com/airhacks/headlands/sync/SyncPresenter.java
@@ -1,6 +1,12 @@
 package com.airhacks.headlands.sync;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javax.inject.Inject;
 
@@ -8,7 +14,7 @@ import javax.inject.Inject;
  *
  * @author airhacks.com
  */
-public class SyncPresenter {
+public class SyncPresenter implements Initializable {
 
     @FXML
     TextField input;
@@ -16,13 +22,25 @@ public class SyncPresenter {
     @FXML
     TextField output;
 
+    @FXML
+    Button syncButton;
+
+    @FXML
+    Label fromInfo;
+
     @Inject
     CacheService cacheService;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        BooleanBinding fieldsAreEquals = input.textProperty().isEqualTo(output.textProperty());
+        syncButton.disableProperty().bind(fieldsAreEquals);
+    }
+
     public void sync() {
-        String inputText = input.getText();
+        String hostFrom = input.getText();
         String outputText = output.getText();
-        cacheService.sync(inputText, outputText);
+        cacheService.sync(fromInfo::setText, hostFrom, outputText);
     }
 
 }
