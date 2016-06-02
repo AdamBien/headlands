e918c050825ea6c7e179caf0fccb17dc4a5235ec
Adam Bien <abien@adam-bien.com>
2016-06-02 11:53:01 +0200

    Event system implemented

--- headlands/src/main/java/com/airhacks/headlands/notifications/boundary/NotificationsEdge.java
+++ headlands/src/main/java/com/airhacks/headlands/notifications/boundary/NotificationsEdge.java
@@ -2,10 +2,12 @@ package com.airhacks.headlands.notifications.boundary;
 
 import com.airhacks.headlands.cache.control.Initializer;
 import com.airhacks.headlands.notifications.control.CacheEntryChangedListener;
+import com.airhacks.headlands.notifications.entity.CacheChangedEvent;
 import javax.cache.Cache;
 import javax.cache.configuration.Factory;
 import javax.cache.configuration.FactoryBuilder;
 import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
 /**
@@ -17,12 +19,15 @@ public class NotificationsEdge {
     @Inject
     Initializer discoverer;
 
+    @Inject
+    Event<CacheChangedEvent> event;
+
     public void registerListener(String cacheName) {
         Cache<Object, Object> cache = this.discoverer.cacheManager().getCache(cacheName);
         if (cache == null) {
             return;
         }
-        CacheEntryChangedListener listener = new CacheEntryChangedListener(cacheName);
+        CacheEntryChangedListener listener = new CacheEntryChangedListener(cacheName, event::fire);
         Factory<CacheEntryChangedListener> listenerFactory = FactoryBuilder.factoryOf(listener);
         MutableCacheEntryListenerConfiguration configuration = new MutableCacheEntryListenerConfiguration(listenerFactory, null, false, true);
         cache.registerCacheEntryListener(configuration);
